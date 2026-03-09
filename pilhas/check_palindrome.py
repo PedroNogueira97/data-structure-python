d9bb7a50debb6832337ea96146dd11909cb0ae22
Pedro Nogueira <pedronogueiramoraes@hotmail.com>
2026-03-09 11:15:58 -0300

    Exercise of stack - Palindrome

---
+++ pilhas/check_palindrome.py
@@ -0,0 +1,16 @@
+from stack import Stack
+
+def check_palindrome(word):
+    stack = Stack()
+    for letter in word.lower():
+        stack.push(letter)
+    
+    reversed_word = ""
+    while not stack.is_empty():
+        reversed_word += stack.pop()
+    
+    palindrome = word.lower() == reversed_word
+    return f"The word {word} is {'a' if palindrome else 'not a'} palindrome"
+
+print(check_palindrome("Madam"))
+print(check_palindrome("hello"))

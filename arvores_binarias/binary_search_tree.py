541f5c2bcbda673650a012c2a446d9f89fb51631
Pedro Nogueira <pedronogueiramoraes@hotmail.com>
2026-03-17 14:27:27 -0300

    Fix BinarySearchTree implementation

--- arvores_binarias/binary_search_tree.py
+++ arvores_binarias/binary_search_tree.py
@@ -7,12 +7,29 @@ class BinarySearchTree:
     def insert(self, key):
         if self.root is None:
             self.root = Node(key)
-        elif key < self.root.key:
-            if self.root.left is None:
-                self.root.left = Node(key)
-            else:
-                self.insert(self.root.left, key)
-        elif self.root.right is None:
-            self.root.right = Node(key)
         else:
-            self.insert(self.root.right, key)
+            self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if key < node.key:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert(node.left, key)
+        else:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert(node.right, key)
+
+if __name__ == "__main__":
+    bst = BinarySearchTree()
+    bst.insert(10)
+    bst.insert(5)
+    bst.insert(15)
+    bst.insert(2)
+    
+    print("BST root:", bst.root.key)
+    print("BST left:", bst.root.left.key)
+    print("BST right:", bst.root.right.key)
+    print("BST left-left:", bst.root.left.left.key)

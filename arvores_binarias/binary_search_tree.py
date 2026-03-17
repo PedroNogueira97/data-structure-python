3ad3798b87d7006e923014caf3a17f1b7f179194
Pedro Nogueira <pedronogueiramoraes@hotmail.com>
2026-03-17 15:11:07 -0300

    Update BST with inorder traversal and stubs for other methods

--- arvores_binarias/binary_search_tree.py
+++ arvores_binarias/binary_search_tree.py
@@ -22,14 +22,53 @@ class BinarySearchTree:
             else:
                 self._insert(node.right, key)
 
+    def inorder(self):
+        return self._inorder(self.root)
+
+    def _inorder(self, node):
+        if node is None:
+            return []
+        return self._inorder(node.left) + [node.key] + self._inorder(node.right)
+
+    #Abaixo metodos a serem implementados
+    def search(self, key):
+        ...
+
+    def _search(self, node, key):
+        ...
+
+    def preorder(self):
+        ...
+
+    def _preorder(self, node):
+        ...
+
+    def postorder(self):
+        ...
+
+    def _postorder(self, node):
+        ...
+
+    def height(self):
+        ...
+
+    def _height(self, node):
+        ...
+
+    def remove(self, key):
+        ...
+
+    def _remove(self, node, key):
+        ...
+
+
 if __name__ == "__main__":
     bst = BinarySearchTree()
-    bst.insert(10)
-    bst.insert(5)
-    bst.insert(15)
-    bst.insert(2)
+    values = [69, 48, 42, 6, 1, 100, 72]
+    for value in values:
+        bst.insert(value)
     
-    print("BST root:", bst.root.key)
-    print("BST left:", bst.root.left.key)
-    print("BST right:", bst.root.right.key)
-    print("BST left-left:", bst.root.left.left.key)
+    print(bst.inorder())
+
+
+    
